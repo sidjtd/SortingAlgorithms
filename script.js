61244ec33118f2e703b8dfc4d180fa9d1b77d0a4
Tyler Chong <tylrchong@gmail.com>
2016-06-28 19:11:44 -1000

    Working graph

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
 function sorted(arr) {
   for(var i = 0; i < arr.length; i++) {
     if(typeof arr[i] !== 'number') {
@@ -5,6 +8,7 @@ function sorted(arr) {
     }
   }
   var sorted = false;
+  var arrays = [arr.slice()];
   while(sorted === false) {
     var swaps = 0;
     for(i = 1; i < arr.length; i++) {
@@ -13,14 +17,30 @@ function sorted(arr) {
         arr[i] = arr[i-1];
         arr[i-1] = temp;
         swaps++;
+        arrays.push(arr.slice());
       }
     }
     if(swaps === 0) {
       sorted = true;
     }
   }
+  var arrayLength = arrays.length;
+  var timer = window.setInterval(function() {
+    if(arrays.length === 1) {
+      window.clearInterval(timer);
+    }
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for(var i = 0;i < arrays[0].length;i++) {
+      ctx.beginPath();
+      ctx.moveTo((i * 20) + canvas.width / arrayLength, canvas.height);
+      ctx.lineWidth = 5;
+      ctx.lineTo((i * 20) + canvas.width / arrayLength, canvas.height - (arrays[0][i] * canvas.height / (canvas.height / 2)));
+      ctx.stroke();
+    }
+    document.getElementById('content').innerHTML = arrays[0];
+    arrays.shift();
+  }, 200);
   return arr;
 }
-Array.sort = sorted;
-module.exports = sorted;
+sorted([100,55,27,109,121,123,21,20,71,12,78,120,99,80,15,20,19,77,120,120,120,250,350,250,300,25,190,260,170,80,40,20,15,170,200,150]);
 
