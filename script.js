008ef3369dd3dab256b3b9e442d377d6c3a04d6e
Tyler Chong <tylrchong@gmail.com>
2016-06-28 14:48:35 -1000

    Cleaned up the code

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
-function sorted() {
-  function sort(arr) {
-    for(var i = 0; i < arr.length; i++) {
-      if(typeof arr[i] !== 'number') {
-        throw new Error('Invalid element in array.');
-      }
+function sorted(arr) {
+  for(var i = 0; i < arr.length; i++) {
+    if(typeof arr[i] !== 'number') {
+      throw new Error('Invalid element in array.');
     }
-    var sorted = false;
-    while(sorted === false) {
-      var swaps = 0;
-      for(i = 1; i < arr.length; i++) {
-        if(arr[i] < arr[i-1]) {
-          var temp = arr[i];
-          arr[i] = arr[i-1];
-          arr[i-1] = temp;
-          swaps++;
-        }
-        console.log(arr);
-      }
-      if(swaps === 0) {
-        sorted = true;
-      }
-    }
-    return arr;
   }
-  return {
-    sort: sort,
-  };
+  var sorted = false;
+  while(sorted === false) {
+    var swaps = 0;
+    for(i = 1; i < arr.length; i++) {
+      if(arr[i] < arr[i-1]) {
+        var temp = arr[i];
+        arr[i] = arr[i-1];
+        arr[i-1] = temp;
+        swaps++;
+      }
+    }
+    if(swaps === 0) {
+      sorted = true;
+    }
+  }
+  return arr;
 }
-
+Array.sort = sorted;
 module.exports = sorted;
+
--- test/script.test.js
+++ test/script.test.js
@@ -5,17 +5,16 @@ var expect = chai.expect;
 chai.should();
 var bubbleSort = require('../script.js');
 
-var test = bubbleSort();
-
 describe('Testing the sorting algorithm.', function () {
   it('Should return the sorted array.', function() {
-    expect(test.sort([1,8,5,2])).to.deep.equal([1,2,5,8]);
-    expect(test.sort([1])).to.deep.equal([1]);
-    expect(test.sort([2,2,2])).to.deep.equal([2,2,2]);
+    expect(bubbleSort([1,8,5,2])).to.deep.equal([1,2,5,8]);
+    expect(bubbleSort([1])).to.deep.equal([1]);
+    expect(bubbleSort([2,2,2])).to.deep.equal([2,2,2]);
+    expect(bubbleSort([12,11,10])).to.deep.equal([10,11,12]);
   });
   it('Should throw an error if an array slot is not a number', function () {
-    expect(test.sort.bind(['a',3,2])).to.throw(Error);
-    expect(test.sort.bind([3,2,5,'$'])).to.throw(Error);
-    expect(test.sort.bind(['12',2,5])).to.throw(Error);
+    expect(bubbleSort.bind(['a',3,2])).to.throw(Error);
+    expect(bubbleSort.bind([3,2,5,'$'])).to.throw(Error);
+    expect(bubbleSort.bind(['12',2,5])).to.throw(Error);
   });
 });
